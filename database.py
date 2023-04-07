d5fafd993c0c4cdc69f3a9ac5debc30a4e3f5576
dd-devq <dd.devq@gmail.com>
2023-04-07 19:58:23 +0700

    nothing

--- database.py
+++ database.py
@@ -10,7 +10,6 @@ def construct_datebase(sql_script, database=default_database):
     conn = sqlite3.connect(database)
     with open(sql_script, 'r') as sql_file:
         queries = sql_file.read()
-        print(queries)
     conn.executescript(queries)
     conn.commit()
     conn.close()
@@ -20,13 +19,36 @@ def query(query, args=(), one=False):
     with sqlite3.connect(default_database) as conn:
         cursor = conn.cursor()
         if query.startswith('SELECT'):
-            cursor.execute(query, args)
-            rv = [dict((cursor.description[idx][0], value)
-                       for idx, value in enumerate(row)) for row in cursor.fetchall()]
-            return (rv[0] if rv else None) if one else rv
+            try:
+                cursor.execute(query, args)
+                rv = [dict((cursor.description[idx][0], value)
+                           for idx, value in enumerate(row)) for row in cursor.fetchall()]
+                return (rv[0] if rv else None) if one else rv
+            except sqlite3.IntegrityError as error:
+                print(err)
+                return None
         else:
-            cursor.execute(query, args)
-            conn.commit()
+            try:
+                cursor.execute(query, args)
+                conn.commit()
+            except sqlite3.IntegrityError as error:
+                print(error)
+    conn.close()
+
+
+def login(username, password):
+    user = query(
+        'SELECT username, password, role FROM user WHERE username = ?', (username,), one=True)
+    print(user)
+
+
+def register(username, password, role='unprivileged'):
+    query('INSERT INTO user (username, password, role) VALUES (?, ?, ?)',
+          (username, password, role))
+
+
+def delete_user(username):
+    query('DELETE FROM user WHERE username = ?', (username,))
 
 
 def login(username, password):
--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from app import *
 if __name__ == "__main__":
     database_folder()
     construct_datebase('scripts/devices.sql')
-    register('test', 'password', 'privileged1')
+    register('test', 'password', 'privileged')
     login('test', 'password')
     delete_user('username')
     # app.run(host='0.0.0.0', port=5000, debug=False, use_evalex=False)
